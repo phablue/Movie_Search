44d9edc4c11f79198d9761567034490e13393249
hyunahpark <phablue@gmail.com>
2015-07-14 10:39:16 -0700

    refacotring search controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,16 +4,17 @@ class MoviesController < ApplicationController
   end
 
   def search
-    search_word = params["q"].strip
-    @movies = search_results(search_word)
+    search_term = params["q"].strip
+    @movies = search_results(search_term)
+    flash[:notice] = search_result_message(@movies.size, search_term)
   end
 
-  def search_results (search_word)
-    if unavailable?(search_word)
-      flash[:error] = errorMessage
+  def search_results (search_term)
+    if unavailable?(search_term)
+      flash[:error] = error_message
       redirect_to "/"
     else
-      Movie.all.select(&included?(search_word))
+      Movie.all.select(&included?(search_term))
     end
   end
 
@@ -22,15 +23,19 @@ class MoviesController < ApplicationController
   end
 
   private
-  def unavailable? (search_word)
-    search_word.nil? || search_word.length < 2
+  def unavailable? (search_term)
+    search_term.nil? || search_term.length < 2
   end
 
-  def included? (search_word)
-    Proc.new {|movie| movie.title.include?(search_word)}
+  def included? (search_term)
+    Proc.new {|movie| movie.title.include?(search_term)}
   end
 
-  def errorMessage
+  def error_message
     "The search word must be more than 2 characters."
+  end
+
+  def search_result_message (result_count, search_term)
+    "Found #{result_count} movies according to the search term '#{search_term}'"
   end
 end
