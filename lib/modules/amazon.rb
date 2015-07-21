da70d0ccfe391656c53c19b826b5a704d0808526
Hana Park <phablue@gmail.com>
2015-07-21 13:07:50 -0700

    setting for amazon API auth

--- lib/modules/amazon.rb
+++ lib/modules/amazon.rb
@@ -2,14 +2,15 @@ module Amazon
   class Movies
     def initialize
       @request = Vacuum.new
-
+      configure(@request)
     end
 
+    private
     def configure (request)
       request.configure(
-        aws_access_key_id: 'key',
-        aws_secret_access_key: 'secret',
-        associate_tag: 'tag'
+        aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+        aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+        associate_tag: ENV["AWS_ASSOCIATE_TAG"]
       )
     end
   end
