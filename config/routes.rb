c71e148b044dc0074a8907e71734bca1a3950800
Hana Park <phablue@gmail.com>
2015-08-23 21:13:58 -0700

    change routs for jasmine

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 
   devise_scope :user do
     get "sign-in", to: "devise/sessions#new"
