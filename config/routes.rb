6a9d589032d88fa8b492c405aa8708269118a604
Hana Park <phablue@gmail.com>
2015-08-24 11:53:37 -0700

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
 
-  devise_scope :user do
-    get "sign-in", to: "devise/sessions#new"
-  end
+  devise_for :users, path: "", 
+              path_names: {
+              sign_in: 'sign-in', sign_out: 'sign-out', password: 'secret',
+              confirmation: 'verification', unlock: 'unblock', registration: 'register',
+              sign_up: 'sign-up' }
 
   controller :movies do
-    get 'result', action: :result
-    get 'movies/:id', action: :show, as: 'movie'
+    get 'result',     action: :result
+    get 'movies/:id', action: :show   , as: 'movie'
   end
 
   controller :favorites do
-    get 'my-list', action: :index
-    post 'my-list', action: :create
+    get 'my-list',    action: :index
+    post 'my-list',   action: :create
     delete 'my-list', action: :destroy
   end
 
