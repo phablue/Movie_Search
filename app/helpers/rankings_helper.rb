5a7e17ee24ce0a1ec821ebeb26b08b988929d86d
Hana Park <phablue@gmail.com>
2015-09-10 11:50:31 -0700

    changed files

--- app/helpers/rankings_helper.rb
+++ app/helpers/rankings_helper.rb
@@ -1,6 +1,17 @@
 module RankingsHelper
-  def current_year
-    Time.now.year.to_i
+  def released_years
+    years = []
+    current_year = Time.now.year.to_i
+
+    (1880..current_year).each { |y| years << y }
+    years.reverse
+  end
+
+  def movie_genres
+    ['Action', 'Adventure', 'Animation', 'Biography', 'Comedy', 'Crime',
+     'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'History', 'Horror',
+     'Music', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Sport', 'Thriller', 'War',
+     'Western']
   end
 end
 
