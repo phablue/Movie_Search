6752baa730407bb47e7bd327b4abcad97a567aef
Hana Park <phablue@gmail.com>
2015-09-11 12:18:32 -0700

    change css in ranking page

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -2,4 +2,8 @@ module FavoritesHelper
   def movie_poster(my_list)
     Movie.find(my_list.movie_id).poster
   end
+
+  def movie_title(my_favorite)
+    Movie.find(my_favorite.movie_id).title
+  end
 end
